2152329c535d0c55c43baf472e03f585d9682ed8
Amal Khalaf <amkhalaf@microsoft.com>
2018-02-14 12:55:34 -0800

    vsts build scripts

---
+++ releaseBuild/Images/microsoft_dsc_ubuntu16.04/test.ps1
@@ -0,0 +1,8 @@
+﻿[cmdletbinding(DefaultParameterSetName='default')]
+# PowerShell Script to clone, build and package PowerShell from specified fork and branch
+param (
+    [string] $input = 'hello-dsc'
+)
+
+Write-Verbose "input param : $input"
+
